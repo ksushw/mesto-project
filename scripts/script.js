3fb6112dff83e9e59474b1e969291eb22244a58b
ksushw <xenianevarenyh@yandex.ru>
2023-03-03 04:04:00 +0300

    add validation form

--- scripts/script.js
+++ scripts/script.js
@@ -72,7 +72,7 @@ profileEdit.addEventListener('click', openPopupEdit);
 
 closeButtons.forEach(function (button) {
   const popup = button.closest('.popup');
-  button.addEventListener('click', function(){
+  button.addEventListener('click', function () {
     togglePopup(popup)
   });
 });
@@ -158,3 +158,73 @@ function handleImgOpen(link, title, card, alt) {
     togglePopup(popupImg)
   })
 }
+
+//Валидация
+const showInputError = (formElement, inputElement, validationMessage) => {
+  const inputEror = formElement.querySelector(`.${inputElement.id}-error`);
+  inputEror.textContent = validationMessage;
+
+  // Нужно ли??
+  inputEror.classList.add = ('form__input-error_active')
+  inputElement.classList.add('.form__input-error');
+}
+
+const hideInputError = (formElement, inputElement) => {
+  const inputEror = formElement.querySelector(`.${inputElement.id}-error`);
+  inputEror.textContent = '';
+
+  // Нужно ли??
+  inputEror.classList.remove('form__input-error_active')
+  inputElement.classList.remove('.form__input-error');
+}
+
+const checkInputValidity = (formElement, inputElement) => {
+  if(inputElement.validity.patternMismatch){
+    inputElement.setCustomValidity('Поля могут содержать только латинские буквы, кириллические буквы, знаки дефиса и пробелы.');
+  } else {
+    inputElement.setCustomValidity('');
+  }
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage)
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}
+
+const enableButton = (inputList, button) => {
+  if (hasInvalidInput(inputList)) {
+    button.classList.add('button_inactive')
+  } else {
+    button.classList.remove('button_inactive')
+  }
+}
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'))
+  const button = formElement.querySelector('.popup__button-save')
+  enableButton(inputList, button)
+
+  inputList.forEach(function(input) {//maybe there is error function report
+    input.addEventListener('change', () => {
+      checkInputValidity(formElement, input);
+      enableButton(inputList, button)
+    })
+  })
+}
+
+const enableValidation = () => {
+  const forms = Array.from(document.querySelectorAll('.popup__form'))
+  forms.forEach((form) => {
+    setEventListeners(form)
+    console.log(form)
+  })
+}
+
+enableValidation()
