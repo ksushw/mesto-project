967310da1d0ed4f291f320762ba72cbaf8b65e41
ksushw <xenianevarenyh@yandex.ru>
2023-03-04 08:50:06 +0300

    edit overlay

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './../src/index.css'
 import { togglePopup } from './components/utils'
-import { popupAdd, createCard, addCard, handleAddFormSubmit, renderCards, handleLike, deleteCard, handleDeleteCard, handleImgOpen } from './components/card'
+import { popupAdd, handleAddFormSubmit, renderCards } from './components/card'
 import { setDisableButton, setProfileDataInInpur, openPopupEdit, popupCloseHandler, handleProfileFormSubmit } from './components/modal'
-import { showInputError, hideInputError, checkInputValidity, hasInvalidInput, enableButton, setEventListeners, enableValidation } from './components/validate'
+import { enableValidation } from './components/validate'
 
 
 const profileEdit = document.querySelector('.profile__edit');
@@ -12,14 +12,35 @@ const addForm = document.forms["add-picture"];
 
 profileEdit.addEventListener('click', openPopupEdit);
 
-
 profileForm.addEventListener('submit', handleProfileFormSubmit);
-
 
 addButton.addEventListener('click', function () {
     togglePopup(popupAdd);
     setDisableButton(popupAdd)
 });
+
+
+const closePopupKaydowAndOverlay = (() => {
+    document.addEventListener('keydown', function (evt) {
+        const openPopup = document.querySelector('.popup_opened')
+        if (evt.keyCode === 27 && openPopup) {
+            openPopup.classList.remove('popup_opened');
+        }
+    })
+    const overlays = Array.from(document.querySelectorAll('.popup__overlay'))
+    overlays.forEach((overlay) => {
+        overlay.addEventListener('click', () => {
+            const openPopup = document.querySelector('.popup_opened')
+            openPopup.classList.remove('popup_opened');
+        })
+
+    })
+
+
+})
+
+closePopupKaydowAndOverlay()
+
 
 addForm.addEventListener('submit', handleAddFormSubmit);
 
